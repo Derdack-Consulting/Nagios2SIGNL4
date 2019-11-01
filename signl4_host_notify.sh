6d65c342cba95d0bc1e0a0a3dde1232fe00b98e6
Derdack Consulting <53823835+Derdack-Consulting@users.noreply.github.com>
2019-11-01 11:00:36 +0100

    Example removed

--- signl4_host_notify.sh
+++ signl4_host_notify.sh
@@ -3,13 +3,6 @@
 # Edit your SIGNL4 hook URL
 SIGNL4_URL=https://connect.signl4.com/webhook/xxxxxxxx
 
-# Host Notification command example :
-
-# define command {
-#	command_name	signl4-host
-#	command_line	/usr/lib/nagios/plugins/signl4_host_notify "$NOTIFICATIONTYPE$" "$HOSTNAME$" "$HOSTADDRESS$" "$HOSTSTATE$" "$HOSTOUTPUT$" "$LONGDATETIME$"
-#}
-
 IFS='%'
 
 SIGNL4_MSG="{ \"Title\": \"Host $1 notification\", \"Host\": \"$2\", \"IP\": \"$3\", \"State\": \"$4\", \"Additional Info\":\"$5\", \"Nagios notification\": \"$6\", \"X-S4-Service\": \"Nagios\" }"
